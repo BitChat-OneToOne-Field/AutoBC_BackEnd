d9fba76c6e8a81a09f7a9182e0b6cbe0c09910e9
georgetoloraia <gtoloraia19@gmail.com>
2024-06-23 11:12:00 +0400

    add: filed | transaction_type |

--- userDashboard/models.py
+++ userDashboard/models.py
@@ -5,6 +5,7 @@ class Transaction(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.DecimalField(max_digits=20, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
+    transaction_type = models.CharField(max_length=10, choices=[('deposit', 'Deposit'), ('withdraw', 'Withdraw')], default='deposit')
 
     def __str__(self):
-        return f"{self.user.email} - {self.amount} - {self.timestamp}"
+        return f"{self.user.email} - {self.amount} - {self.timestamp} - {self.transaction_type}"
