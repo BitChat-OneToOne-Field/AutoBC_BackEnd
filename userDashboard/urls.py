3a72945c9f6b4d2fbf20a3b45763b56c2ce29bd7
georgetoloraia <gtoloraia19@gmail.com>
2024-06-23 11:19:20 +0400

    modified and add: url 'deposit/' in userDashboard

--- userDashboard/urls.py
+++ userDashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserDashboardView
+from .views import UserDashboardView, DepositView
 
 urlpatterns = [
     path('userDashboard/', UserDashboardView.as_view(), name='user-dashboard'),
+    path('deposit/', DepositView.as_view(), name='deposit'),
 ]
